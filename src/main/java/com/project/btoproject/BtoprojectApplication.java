0e8ed0e9694028f997d6624803c44d5393c2f1f2
Elif Ece Can <ececan@ug.bilkent.edu.tr>
2024-10-03 22:56:08 +0300

    feat: setup spring application

---
+++ src/main/java/com/project/btoproject/BtoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.project.btoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtoprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/btoproject/BtoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.btoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
