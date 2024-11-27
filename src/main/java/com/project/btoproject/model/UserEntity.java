24d97690e476daf49cf50127b243b98369755b1a
aycaatac <114350398+aycaatac@users.noreply.github.com>
2024-11-27 12:26:23 +0300

    fix: fix imports

--- src/main/java/com/project/btoproject/model/UserEntity.java
+++ src/main/java/com/project/btoproject/model/UserEntity.java
@@ -3,7 +3,7 @@ package com.project.btoproject.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
