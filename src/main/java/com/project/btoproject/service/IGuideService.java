0fee48c909b170fd7cfe3b8026b7a9a48faa3d31
eececan <ececan@ug.bilkent.edu.tr>
2024-11-28 19:52:06 +0300

    feat: Implement GuideService version one

--- src/main/java/com/project/btoproject/service/IGuideService.java
+++ src/main/java/com/project/btoproject/service/IGuideService.java
@@ -2,11 +2,14 @@ package com.project.btoproject.service;
 
 import com.project.btoproject.model.*;
 
+import java.util.List;
+
 public interface IGuideService {
-    Event[] seeAssignedEvents();
+    void saveGuide(Guide guide);
+    List<Event> seeAssignedEvents(Guide g);
     Advisor seeAdvisorOfDay();
-    void selfAssignTour(Tour t);
-    void selfAssignIndividualTour(IndividualTour t);
+    void selfAssignTour(Guide guide, Tour tour);
+    void selfAssignIndividualTour(Guide guide, IndividualTour tour);
     int seeCurrentPoints(Guide g);
 
 }
