4b9fb8c45123d2466a5cb62f0519e72ad3d15b31
eececan <ececan@ug.bilkent.edu.tr>
2024-11-28 19:52:06 +0300

    feat: Implement GuideService version one

--- src/main/java/com/project/btoproject/service/GuideService.java
+++ src/main/java/com/project/btoproject/service/GuideService.java
@@ -1,10 +1,13 @@
 package com.project.btoproject.service;
 
+import com.project.btoproject.common.PointRecord;
 import com.project.btoproject.model.*;
 import com.project.btoproject.repository.IGuideRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,26 +16,44 @@ public class GuideService implements IGuideService
 {
     private final IGuideRepository guideRepository;
 
-    public Event[] seeAssignedEvents() {
-        //TODO 28.11
-        return null;
+    public void saveGuide(Guide guide) {
+        guideRepository.save(guide);
+    }
+
+    public List<Event> seeAssignedEvents(Guide guide) {
+        List<Event> events = guide.getEvents();
+        return events;
+    }
+
+    @Transactional
+    public void selfAssignTour(Guide guide, Tour tour) {
+        if (!tour.getGuides().contains(guide)) {
+            tour.getGuides().add(guide);
+            guide.getEvents().add(tour);
+            guideRepository.save(guide);
+        }
+    }
+
+    @Transactional
+    public void selfAssignIndividualTour(Guide guide, IndividualTour individualTour) {
+        if(individualTour.getGuide()!=null) {
+            throw new IllegalStateException("This individual tour already has a guide assigned.");
+        }
+        individualTour.setGuide(guide);
+        guide.getEvents().add(individualTour);
+        guideRepository.save(guide);
+    }
+
+    public int seeCurrentPoints(Guide guide) {
+        int totalPoints = 0;
+        for(PointRecord pointRecord : guide.getPoints()) {
+            totalPoints += pointRecord.getPoint();
+        }
+        return totalPoints;
     }
 
     public Advisor seeAdvisorOfDay() {
-        //TODO 28.11
+        //TODO
         return null;
-    }
-
-    public void selfAssignTour(Tour t) {
-        //TODO 28.11
-    }
-
-    public void selfAssignIndividualTour(IndividualTour t) {
-        //TODO 28.11
-    }
-
-    public int seeCurrentPoints(Guide g) {
-        //TODO 28.11
-        return 0;
     }
 }
