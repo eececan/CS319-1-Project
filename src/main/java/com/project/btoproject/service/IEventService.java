603b00338595668316d1482af21637b22e00c7bd
eececan <ececan@ug.bilkent.edu.tr>
2024-12-03 23:32:06 +0300

    feat: Implement fair related service methods

--- src/main/java/com/project/btoproject/service/IEventService.java
+++ src/main/java/com/project/btoproject/service/IEventService.java
@@ -1,11 +1,18 @@
 package com.project.btoproject.service;
 
 import com.project.btoproject.enums.Status;
+import com.project.btoproject.model.Event;
+import com.project.btoproject.model.Fair;
 import com.project.btoproject.model.Tour;
 import com.project.btoproject.model.TourInfo;
+
+import java.util.List;
 
 public interface IEventService {
     void setStatusOfTour(Tour t, Status status);
     void changeTourInformation(Tour t, TourInfo tourInfo);
-
+    void changeFairInformation(Fair f, String fairInfo);
+    List<Fair> seeUpcomingFairs();
+    void sendFairReminderToResponsibleMembers();
+    String seeRemainingTimeUntilEvent(Event e);
 }
