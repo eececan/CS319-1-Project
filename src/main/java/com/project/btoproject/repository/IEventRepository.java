b9922d8ec76e63e4d5687519086e7954a935733d
Mustafa Özkan İr <mustafa54ir@gmail.com>
2024-12-06 18:21:46 +0300

    getAllTours and getAllFairs methods are implemented.

--- src/main/java/com/project/btoproject/repository/IEventRepository.java
+++ src/main/java/com/project/btoproject/repository/IEventRepository.java
@@ -1,10 +1,12 @@
 package com.project.btoproject.repository;
 
 import com.project.btoproject.model.Event;
+import com.project.btoproject.model.Tour;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
+import java.util.List;
 
 public interface IEventRepository extends JpaRepository<Event, Long> {
 
@@ -19,5 +21,12 @@ public interface IEventRepository extends JpaRepository<Event, Long> {
      */
     @Query("SELECT MAX(f.applicationTimeStamp) FROM Fair f")
     Date findLatestFairApplicationTimeStamp();
+
+    @Query("SELECT e FROM Event e WHERE TYPE(e) = Tour")
+    List<Tour> findAllTours();
+
+    @Query("SELECT e FROM Event e WHERE TYPE(e) = Fair")
+    List<Tour> findAllFairs();
+
 
 }
