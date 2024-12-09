526cd7d35264d629c654b001084f9dc0f97c43ea
Emre Furkan Akyol <e.furkanakyol@gmail.com>
2024-12-09 23:56:00 +0300

    Advisor Method and Data
    Advisor repository Method and data intiializer added

--- src/main/java/com/project/btoproject/repository/IAdvisorRepository.java
+++ src/main/java/com/project/btoproject/repository/IAdvisorRepository.java
@@ -5,8 +5,10 @@ import com.project.btoproject.model.Guide;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.DayOfWeek;
+import java.util.Optional;
 
 public interface IAdvisorRepository extends JpaRepository<Advisor, Integer> {
     Advisor getAdvisorById(Long id);
     Advisor findByResponsibleDay(DayOfWeek day);
+    Optional<Advisor> findByFirstNameAndLastName(String firstName, String lastName);
 }
