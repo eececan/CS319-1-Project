8c768b7df2193e8e99e390eaf4cf4f682fcc09ec
eececan <ececan@ug.bilkent.edu.tr>
2024-12-10 01:14:50 +0300

    fix: correct convention mistake in UserTask

--- src/main/java/com/project/btoproject/repository/IUserTaskRepository.java
+++ src/main/java/com/project/btoproject/repository/IUserTaskRepository.java
@@ -4,5 +4,5 @@ import com.project.btoproject.model.UserTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUserTaskRepository extends JpaRepository<UserTask, Integer> {
-    UserTask getUserTaskById(int id);
+    UserTask getUserTaskByTaskId(int id);
 }
