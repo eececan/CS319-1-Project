0956ec00d6efbf5ce7160cbac3f1b653c9cf1a98
elceren <ceren.celik@ug.bilkent.edu.tr>
2024-12-09 23:39:18 +0300

    getAllUsersAdded

--- src/main/java/com/project/btoproject/security/SecurityConfig.java
+++ src/main/java/com/project/btoproject/security/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
                         .requestMatchers(
 
                                 "/api/auth/**",
+                                "/getAllUsers",
                                 "/getAllGuides",
                                 "/getAllEvents",
                                 "/",
