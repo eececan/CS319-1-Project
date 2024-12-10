851b7a4d5657210b5f7e963076938b0f5dee237d
eececan <ececan@ug.bilkent.edu.tr>
2024-12-10 17:49:25 +0300

    feat: write custom query for getting user tasks

--- src/main/java/com/project/btoproject/repository/IAllUsersRepository.java
+++ src/main/java/com/project/btoproject/repository/IAllUsersRepository.java
@@ -3,9 +3,12 @@ package com.project.btoproject.repository;
 import com.project.btoproject.model.User;
 import com.project.btoproject.model.UserTask;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface IAllUsersRepository extends JpaRepository<User, Long> {
-    List<UserTask> findUserTasksById(Long id);
+    @Query("SELECT ut FROM UserTask ut WHERE ut.user.id = :id")
+    List<UserTask> findUserTasksById(@Param("id") Long id);
 }
