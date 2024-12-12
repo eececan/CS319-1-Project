a7ad3b4796cc779272fe64638bd8d9ef81093810
elceren <ceren.celik@ug.bilkent.edu.tr>
2024-12-13 00:24:48 +0300

    fixed logic to get the statistics

--- src/main/java/com/project/btoproject/repository/UniversityStudentRepository.java
+++ src/main/java/com/project/btoproject/repository/UniversityStudentRepository.java
@@ -3,6 +3,7 @@ package com.project.btoproject.repository;
 import com.project.btoproject.model.HighSchoolForStatistics;
 import com.project.btoproject.model.UniversityStudentForStatistics;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,10 @@ import java.util.List;
 @Repository
 public interface UniversityStudentRepository extends JpaRepository<UniversityStudentForStatistics, Long> {
 
-    // Custom query to count students for a particular high school
-    List<Object[]> countStudentsByHighSchool(HighSchoolForStatistics highSchoolForStatistics);
+    // Query to get statistics of high schools and number of students who are enrolled at Bilkent University
+    @Query("SELECT hs.name, COUNT(u) FROM UniversityStudentForStatistics u " +
+            "JOIN u.highSchool hs " +
+            "GROUP BY hs.name " +
+            "ORDER BY COUNT(u) DESC")
+    List<Object[]> countStudentsByHighSchool();
 }
