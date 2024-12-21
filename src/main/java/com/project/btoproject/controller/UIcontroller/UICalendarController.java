86a65a90e6aa4329d62930d226f7bfe8034f91a6
elceren <ceren.celik@ug.bilkent.edu.tr>
2024-12-21 21:05:10 +0300

    design fixes for calender

--- src/main/java/com/project/btoproject/controller/UIcontroller/UICalendarController.java
+++ src/main/java/com/project/btoproject/controller/UIcontroller/UICalendarController.java
@@ -2,12 +2,14 @@ package com.project.btoproject.controller.UIcontroller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.btoproject.model.Event;
-import com.project.btoproject.model.Fair;
-import com.project.btoproject.model.IndividualTour;
-import com.project.btoproject.model.Tour;
+import com.project.btoproject.model.*;
+import com.project.btoproject.service.AllUsersService;
 import com.project.btoproject.service.EventService;
+import com.project.btoproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,14 +25,41 @@ import java.util.stream.Collectors;
 public class UICalendarController {
 
     private final EventService eventService;
+    private final AllUsersService allUsersService;
 
     @Autowired
-    public UICalendarController(EventService eventService) {
+    public UICalendarController(EventService eventService, AllUsersService allUsersService) {
         this.eventService = eventService;
+        this.allUsersService = allUsersService;
     }
 
     @GetMapping("/calendar")
-    public String calendar() {
+    public String calendar(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = "";
+        String roleUser="";
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            username = userDetails.getUsername();
+        }
+        User user = allUsersService.getUserById(Long.parseLong(username)).get();
+        model.addAttribute("user", user);
+        if (authentication.getAuthorities().stream().anyMatch(role -> role.getAuthority().equals("ROLE_DIRECTOR"))) {
+            roleUser ="DIRECTOR";
+            model.addAttribute("roleUser", roleUser);
+        }
+        else if (authentication.getAuthorities().stream().anyMatch(role -> role.getAuthority().equals("ROLE_GUIDE")) || (authentication.getAuthorities().stream().anyMatch(role -> role.getAuthority().equals("ROLE_GUIDE_IN_TRAINING")) )) {
+            roleUser ="GUIDE";
+            model.addAttribute("roleUser", roleUser);
+        }
+        else if (authentication.getAuthorities().stream().anyMatch(role -> role.getAuthority().equals("ROLE_ADVISOR"))) {
+            roleUser ="ADVISOR";
+            model.addAttribute("roleUser", roleUser);
+        }
+        else{
+            roleUser ="HEAD SECRETARY";
+            model.addAttribute("roleUser", roleUser);
+        }
         // Return the calendar view, but don't send events directly in the model
         return "app-calendar";  // The calendar page is rendered normally
     }
@@ -39,6 +68,7 @@ public class UICalendarController {
     @GetMapping("/api/events")
     @ResponseBody
     public List<Map<String, Object>> getEvents() {
+
         List<Event> events = eventService.getAllEvents();
 
         // Convert events to a format suitable for FullCalendar
