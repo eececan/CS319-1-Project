20a37cb60ee2c1bb7dae86c8d22c22a9293ade0f
eececan <ececan@ug.bilkent.edu.tr>
2024-12-23 04:13:29 +0300

    feat: separate incident report pages for roles

--- src/main/java/com/project/btoproject/controller/UIcontroller/UIIncidentController.java
+++ src/main/java/com/project/btoproject/controller/UIcontroller/UIIncidentController.java
@@ -4,6 +4,9 @@ import com.project.btoproject.model.IncidentReport;
 import com.project.btoproject.model.PointRecord;
 import com.project.btoproject.service.IIncidentReportService;
 import com.project.btoproject.service.IncidentReportService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +26,19 @@ public class UIIncidentController {
 
     @GetMapping("/getIncidentReports")
     public String getIncidentRecordsPage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = "";
+        String role = "";
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            username = userDetails.getUsername();
+            role = userDetails.getAuthorities()
+                    .stream()
+                    .findFirst()
+                    .map(authority -> authority.getAuthority())
+                    .orElse("ROLE_UNKNOWN");
+            model.addAttribute("role", role);
+        }
         List<IncidentReport> reports = incidentReportService.getAllIncidentReports();
         model.addAttribute("all_reports", reports);
         return "incident-report-list";
